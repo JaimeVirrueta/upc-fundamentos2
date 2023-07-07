b14f2b7098e71974ff48cb145ce86edbbb428aea
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2023-07-07 02:56:31 -0500

    Se implementó la clase tipo servicio para el manejo del menú en pantalla

---
+++ src/TallerVehiculo/Services/Screen.java
@@ -0,0 +1,56 @@
+package TallerVehiculo.Services;
+
+import java.util.Scanner;
+
+public class Screen {
+
+    Scanner sc = new Scanner(System.in);
+
+    public Screen(){};
+
+    public void print(String texto){
+        System.out.println(texto);
+    }
+
+    public void printTitulo(String texto){
+        System.out.println("\u001B[34m\n" + texto + "\u001B[0m");
+    }
+
+    public void printSubtitulo(String texto){
+        System.out.println("\u001B[32m" + texto + "\u001B[0m");
+    }
+
+    public void printAlerta(String texto){
+        System.out.println("\u001B[33m¡" + texto + "!\u001B[0m");
+    }
+
+    public void printCorrecto(String texto){
+        System.out.println("\u001B[36m" + texto + "\u001B[0m");
+    }
+
+    public void printInput(String text){
+        System.out.print(text);
+    }
+
+    public int getInt(String text){
+        this.printInput(text);
+        int value = sc.nextInt();
+        this.sc.nextLine();
+
+        return value;
+    }
+
+    public String getString(String text) {
+        this.printInput(text);
+        String value = this.sc.nextLine();
+
+        return value;
+    }
+
+    public double getDouble(String text) {
+        this.printInput(text);
+        double value = sc.nextDouble();
+
+        return value;
+    }
+}
